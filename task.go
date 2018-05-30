2cb1aa01b5eb4aeaf1865862867998fdee2cc1eb
Steve High <steve.high@gmail.com>
2018-05-30 10:02:32 -0400

    renamed to taskchain

--- task.go
+++ task.go
@@ -1,4 +1,4 @@
-package taskgraph
+package taskchain
 
 // Task is any function that takes in a TaskGroup and returns an error
 type Task func(*TaskGroup) error
--- taskgroup_test.go
+++ taskgroup_test.go
@@ -1,4 +1,4 @@
-package taskgraph
+package taskchain
 
 import (
 	"bytes"
